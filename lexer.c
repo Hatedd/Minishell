fe10941de5594abbeb5bca43c0dea41cfddd8edf
Youssef Benali <yobenali@e2r5p1.1337.ma>
2022-10-19 00:57:16 +0100

    ..

---
+++ lexer.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yobenali <yobenali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 17:05:03 by yobenali          #+#    #+#             */
+/*   Updated: 2022/10/18 22:23:43 by yobenali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int       ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+t_lexer *init_lexer(char *content)
+{
+    t_lexer *lexer;
+
+    lexer = malloc(sizeof(t_lexer));
+    lexer->i = 0;
+    lexer->len = ft_strlen(content);
+    lexer->content = content;
+    lexer->c = content[lexer->i];
+ 
+    return (lexer);
+}
+
+void    lexer_advance(t_lexer *lexer)
+{
+    if (lexer->c != '\0' && lexer->i < lexer->len)   
+    {
+        lexer->i++;
+        lexer->c = lexer->content[lexer->i];
+    }
+}
+
+void    lexer_skip_space(t_lexer *lexer)
+{
+    if (lexer->c == ' ' || lexer->c == '\n')
+    {
+        lexer_advance(lexer);   
+    }
+}
+
+t_lexer *get_next_token(t_lexer *lexer)
+{
+    while (lexer->c != '\0' && lexer->i < lexer->len)
+    {
+        if (lexer->c == ' ' || lexer->c == '\n')
+            lexer_skip_space(lexer);
+    }
+}
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: yobenali <yobenali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 00:05:08 by yobenali          #+#    #+#             */
-/*   Updated: 2022/10/02 01:47:47 by yobenali         ###   ########.fr       */
+/*   Updated: 2022/10/19 00:56:57 by yobenali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,28 @@ typedef struct s_meta
     char    *m_str;
     char    *token;
 }   t_meta;
+
+typedef struct s_lexer
+{
+    char	c;
+    char	*content;
+    int		i;
+	int		len;
+}   t_lexer;
+
+typedef	struct s_token
+{
+	enum
+	{
+		token_id,
+		token_sign,
+		token_string,
+		token_semi,
+		token_lpar,
+		token_rpar
+	};
+	char *content;
+}	t_token;
 
 
 #endif
